3c4e15e706b86d1830e3f3e72cbf7bd1e7d0f3e4
horladimeygee <56276810+horladimeygee@users.noreply.github.com>
2020-06-04 12:34:57 +0100

    Create Hng_01296.js

---
+++ scripts/Hng_01296.js
@@ -0,0 +1,11 @@
+
+let name = "Adeyemi Habeeb";
+let HNGi7_ID = "01296";
+let language = "javascript";
+let email = "untamed737@gmail.com"
+
+let Display = () => {
+  console.log(`Hello World, this is ${name} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task. ${email}`)
+}
+
+Display()
